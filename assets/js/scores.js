0762307480115237da62548f6d0420596389b036
meri <mariana_asavoaiei@outlook.com>
2023-03-14 03:52:43 +0000

    clearing the highscores by setting the innerHTML to empty string.

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -4,3 +4,8 @@ let userInitials = document.createElement('li');
 userInitials.textContent = localStorage.getItem('userInfo');
 highscoresOl.appendChild(userInitials);
 let clearScoresBttn = document.querySelector('#clear');
+
+//clearing the highscores by setting the innerHtml of the li to empty string.
+clearScoresBttn.addEventListener('click', function(){
+    userInitials.innerHTML = "";
+})
